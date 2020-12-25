f28076e18d10613702af0cd96d1cea5d34e8990e
Giovanni Orellana <giovanni.orellana.caminos@gmail.com>
2020-12-25 16:15:06 -0600

    update

--- src/app/servicios/megusta.service.ts
+++ src/app/servicios/megusta.service.ts
@@ -1,19 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { MeGusta } from '../clases/MeGusta';
-import {
-  urlBase,
-  urlLista,
-  urlPublica,
-  urlSave,
-  urlDelete,
-  urlMegusta,
-} from '../clases/urlglobales';
+import { Observable, throwError } from 'rxjs'
+import { urlBase, urlLista, urlPublica, urlSave, urlDelete, urlMegusta,} from '../clases/urlglobales';
 import { map, catchError } from 'rxjs/operators';
 import { Router } from '@angular/router';
 import { AuthService } from '../authorization/auth.service';
 import Swal from 'sweetalert2/dist/sweetalert2.js';
+import { MeGusta } from '../clases/MeGusta';
 
 @Injectable({
   providedIn: 'root',
