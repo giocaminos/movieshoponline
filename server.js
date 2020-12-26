67cf8972ce8b951fbf84529c11d290d1110d395b
Giovanni Orellana <giovanni.orellana.caminos@gmail.com>
2020-12-26 11:36:58 -0600

    update

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/peliculas'));
+app.use(express.static(__dirname+'/dist/ng-blog'));
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/peliculas/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/ng-blog/index.html'));
 });
-app.listen(process.env.PORT || 8080);
